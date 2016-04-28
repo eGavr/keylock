28dabaab99ec4c43bc97fe667b7b0882c7bebdd7
egavr <job.egavr@yandex.ru>
2016-04-29 01:03:19 +0300

    It's a final count down!

--- data/phrases.js
+++ data/phrases.js
@@ -1,17 +1,14 @@
 ///
 module.exports = [
-    // [
-    //     'Your team is only as good as your weakest reviewer'
-    // ],
-    // [
-    //     'A C program is like a fast dance on a newly waxed dance floor by people carrying razors'
-    // ],
-    // [
-    //     'What everyone wants from life is continuous and genuine happiness'
-    // ],
-    // [
-    //     'Your team is only as good as your weakest reviewer'
-    // ],
+    [
+        'Your team is only as good as your weakest reviewer'
+    ],
+    [
+        'A C program is like a fast dance on a newly waxed dance floor by people carrying razors'
+    ],
+    [
+        'What everyone wants from life is continuous and genuine happiness'
+    ],
     [
         'The winds and waves are always on the side of the ablest navigators'
     ]
