665e0f0e2d4266a41aed20bbc767a674edd76876
egavr <job.egavr@yandex.ru>
2016-05-21 13:41:03 +0300

    Save point

--- data/phrases.js
+++ data/phrases.js
@@ -3,13 +3,16 @@ module.exports = [
     [
         'Your team is only as good as your weakest reviewer'
     ],
-    [
-        'A C program is like a fast dance on a newly waxed dance floor by people carrying razors'
-    ],
-    [
-        'What everyone wants from life is continuous and genuine happiness'
-    ],
-    [
-        'The winds and waves are always on the side of the ablest navigators'
-    ]
+    // [
+    //     'Olopo iure jklos vjhyq lodmys vjudieks vkiodl'
+    // ]
+    // [
+    //     'A C program is like a fast dance on a newly waxed dance floor by people carrying razors'
+    // ],
+    // [
+    //     'What everyone wants from life is continuous and genuine happiness'
+    // ],
+    // [
+    //     'The winds and waves are always on the side of the ablest navigators'
+    // ]
 ];
